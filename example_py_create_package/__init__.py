6f1df30ad4ad310ac7b90936c8cfdec4514466b2
ricardo <ricardo.torrejas@gmail.com>
2023-11-23 06:22:53 -0400

    1.0.0

--- example_py_create_package/__init__.py
+++ example_py_create_package/__init__.py
@@ -1,9 +1,8 @@
 from .moduleMain import moduleMain
 from .initVacio import init_vacio
 from example_py_create_package.sinInit import sin_init
-from .bienImportado.bien_importado import bienImportado
-from example_py_create_package.indexRelleno import __index__
-from .a import a
+from .bienImportado import bienImportado
+from example_py_create_package.indexRelleno import index_relleno
 
 bien_importado = {
     "bienImportado": bienImportado
--- example_py_create_package/a/a.py
+++
@@ -1,2 +0,0 @@
-from example_py_create_package.a.b import b
-from .c import c
